aed92296b160365a7094a1ceb60c1392647987a7
George Mendoza <gsmendoza@gmail.com>
2012-04-09 16:54:38 +0800

    [#1] Update App#execute usage info.

--- lib/switch_file/app.rb
+++ lib/switch_file/app.rb
@@ -1,10 +1,15 @@
 module SwitchFile
   class App < Thor
-    desc "run PATH [OPTIONS]...", "Switch to the matching file type of PATH."
+    desc "execute PATH [OPTIONS]...", "Switch to the matching file type of PATH."
 
     method_option "shortcut",
       :aliases => "-s",
       :desc => "the shortcut of the file type you wish to open",
+      :type => :string
+
+    method_option "config",
+      :aliases => "-c",
+      :desc => "configuration path of the project. PRESENT_WORKING_DIRECTORY/.switch_file by default.",
       :type => :string
 
     def execute(source_path)
